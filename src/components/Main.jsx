867edb6bcc9f6afe137b1d7d2ec5360441d90a81
Joel Pitzer <jpitzer5@gmail.com>
2025-01-20 21:50:18 -0800

    made the input forms controlled components and updated the meme text to match the text in the input forms

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,42 @@
+import { useState } from "react"
+
 export default function Main() {
+    const [meme, setMeme] = useState({topText: "One does not simply", bottomText: "Walk into Mordor", imageUrl: "http://i.imgflip.com/1bij.jpg"})
+
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
                 <label>Top Text
                     <input
                         type="text"
-                        placeholder="One does not simply"
+                        placeholder={meme.topText}
                         name="topText"
+                        onChange={handleChange}
                     />
                 </label>
 
                 <label>Bottom Text
                     <input
                         type="text"
-                        placeholder="Walk into Mordor"
+                        placeholder={meme.bottomText}
                         name="bottomText"
+                        onChange={handleChange}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
